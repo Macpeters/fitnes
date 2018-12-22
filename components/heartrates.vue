4e68e37ec4e2ad09f186be34e3d0855074e499da
Macpeters <peters.maryanne@gmail.com>
2018-12-22 13:11:06 -0500

    heartrate component

---
+++ components/heartrates.vue
@@ -0,0 +1,63 @@
+<template>
+  <section class="container">
+    <div class="heartrates">
+      <ul>
+        <li class="item" v-for="(item, index) in day" :key="index">
+          <span class="bar" v-bind:style="{ top: 150 - item.value.bpm + 'px' }" v-bind:title="item.value.bpm">
+            <img class="heartrate-line" src="~/assets/blueLine.svg">
+          </span>
+          <span class="foo">*</span>
+        </li>
+      </ul>   
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    props: [
+      'day'
+    ]
+  }
+</script>
+
+<style scoped>
+  .heartrates {
+    overflow: hidden;
+    height: 150px;
+  }
+
+  ul {
+    white-space: nowrap;
+    overflow: hidden;
+    overflow-x:scroll;
+  }
+
+  li {
+    opacity: 0.5;
+    display: inline-block;
+    list-style: none;
+    position: relative;
+    width: 15px;
+  }
+
+  li:hover {
+    opacity: 1;
+  }
+
+  img {
+    /* margin-left: -100px; */
+    opacity: 0.5;
+  }
+
+ 
+  .bar {
+    position: relative;
+    width: 10px;
+  }
+
+   .foo {
+    top: 140px;;
+    position: relative;
+  }
+</style>
--- pages/heartrate.vue
+++ pages/heartrate.vue
@@ -4,34 +4,28 @@
       <h1 class="title">
         Heart Rate
       </h1>
+      <heartrates :day="Dec03"/>
 
-      <div class="heartrates">
-        <ul>
-          
-          <li class="item" v-for="(item, index) in Dec03" :key="index">
-            <span class="bar" v-bind:style="{ top: 150 - item.value.bpm + 'px' }" v-bind:title="item.value.bpm">
-              *<img class="heartrate-line" src="~/assets/blueLine.svg">
-            </span>
-            <span class="foo">*</span>
-          </li>
-        </ul>  
-      </div>
-
-      <div class="imagetest">
-        IMAGE TEST
-         <img class="blue-line" src="~/assets/blueLine.svg">
-      </div>
+    
 
       <hr/>
-      <h2 class="subtitle">
-        {{Dec03}}
-      </h2>
+      <h5 class="subtitle">
+        <ul>
+          <li v-for="(item, index) in Dec03" :key="index">
+            {{item}}
+          </li>
+        </ul>
+      </h5>
     </div>
   </section>
 </template>
 
 <script>
+  import heartrates from '~/components/heartrates.vue'
   export default {
+    components: {
+      heartrates: heartrates
+    },
     async asyncData({ app, params, error }) {
       let Dec03 = await app.$axios.$get('https://tender-wescoff-5cd4a9.netlify.com/heart_rate-2018-12-03.json')
       return { Dec03 }
@@ -40,74 +34,14 @@
 </script>
 
 <style scoped>
-  .container {
+  .title {
     justify-content: center;
     align-items: center;
     text-align: center;
-  }
-
-  .title {
     display: block;
     font-weight: 300;
     font-size: 100px;
     color: #35495e;
     letter-spacing: 1px;
   }
-
-  .imagetest {
-    background-color:#ddd;
-  }
-
-  .blue-line {
-    position: relative;
-    padding-top: 5px;
-    height: 100px;
-    transform: rotate(10deg);
-  }
-
-  .heartrates {
-    overflow: hidden;
-    width: 100%;
-    height: 150px;
-  }
-
-  ul {
-    white-space: nowrap;
-    overflow: hidden;
-    overflow-x:scroll;
-  }
-
-  li {
-    display: inline-block;
-    list-style: none;
-    position: relative;
-    height: 150px;
-    width: 10px;
-  }
-
-  img {
-    opacity: 0.5;
-  }
-
- 
-  .bar {
-    position: relative;
-  }
-/* 
-  .bar img:nth-child(2n) {
-    transform: rotate(10deg);
-    opacity: 0.5;
-
-  }
-
-  .bar img:nth-child(2n + 1) {
-    transform: rotate(170deg);
-  }
- */
-
-   .foo {
-    top: 140px;;
-    position: relative;
-  }
-
 </style>
